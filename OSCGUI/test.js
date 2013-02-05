5e8d0333536605f52888d0c4c07aff4194ac25f6
Johnny-English <Vincent.vandidden@gmail.com>
2013-02-05 12:58:15 +0100

    Update OSCGUI/test.js

--- OSCGUI/test.js
+++ OSCGUI/test.js
@@ -48,14 +48,7 @@ pages = [
     "isVertical" : true,
 },
 /* mode slider */
-{
-    "name":"modd",
-    "type":"Multibutton",
-    "x":0.03, "y":0.85,
-    "width":0.94, "height":.1,
-    "rows" : 1, "columns" : 15,
-    "bounds": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14],
-},
+
 /* speed slider */
 {
     "name":"delay",
